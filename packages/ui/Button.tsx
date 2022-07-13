19b0166d62bc288347f938529008608faeded408
Son Tran <son.tran@team.enouvo.com>
2022-07-13 15:15:56 +0700

    Init project

---
+++ packages/ui/Button.tsx
@@ -0,0 +1,10 @@
+import { Button as ChakraButton } from '@chakra-ui/react';
+import * as React from 'react';
+
+export const OurButton = () => {
+  return (
+    <ChakraButton className="rounded-full !bg-blue-500 text-white">
+      Boop
+    </ChakraButton>
+  );
+};
